2ca0b7fcc1a0b6bb7a12f60b211ae4eb919f710d
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-12-10 04:42:36 +0530

    fix(search): remove filter functionality that causes lags
    This reverts commit 7dc0fec6b76301b69a970b26eb9966ac7564cf3c
    to address interface lags during search filtering.
    
    Will be reimplemented with performance optimizations.
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- app/src/main/java/dev/jyotiraditya/echoir/domain/usecase/SearchUseCase.kt
+++ app/src/main/java/dev/jyotiraditya/echoir/domain/usecase/SearchUseCase.kt
@@ -2,7 +2,6 @@ package dev.jyotiraditya.echoir.domain.usecase
 
 import dev.jyotiraditya.echoir.domain.model.SearchResult
 import dev.jyotiraditya.echoir.domain.repository.SearchRepository
-import dev.jyotiraditya.echoir.presentation.screens.search.SearchFilter
 import dev.jyotiraditya.echoir.presentation.screens.search.SearchType
 import javax.inject.Inject
 
@@ -14,9 +13,4 @@ class SearchUseCase @Inject constructor(
 
     suspend fun searchAlbums(query: String): List<SearchResult> =
         repository.search(query, SearchType.ALBUMS)
-
-    suspend fun filterSearchResults(
-        results: List<SearchResult>,
-        filter: SearchFilter
-    ): List<SearchResult> = repository.filterSearchResults(results, filter)
 }
